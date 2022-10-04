97d37431c9d91f2b0d65bc6c99c1ec447c868749
rasitcolakel <rasitcolakel@hotmail.com>
2022-10-05 02:44:53 +0300

    🔔 onMessageCreate function added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,35 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
 });
+const db = admin.firestore();
 
+// when message created in firestore
+export const onMessageCreate = functions
+    .region("europe-west1")
+    .firestore
+    .document("chats/{chatId}/messages/{messageId}")
+    .onCreate(async (snap, context) => {
+      // get new value
+      const newValue = snap.data();
+      const chat = await db.collection("chats")
+          .doc(context.params.chatId).get();
+      const members = chat?.data()?.members;
+      console.log("members", members);
+      const receiver = members.
+          find((m: string) => m !== newValue?.senderId);
+
+      if (receiver) {
+        const user = await db.collection("users")
+            .doc(receiver).get();
+        console.log("user", user);
+      }
+      // get a document reference
+      // access a particular field as you would any JS property
+      console.log("newValue", newValue);
+      console.log("context.params", context.params);
+
+      // perform desired operations ...
+    });
